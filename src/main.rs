50ee2f9d24492dff48d5017fc6b04a00d22981d8
BBaoVanC <bbaovanc@bbaovanc.com>
2021-08-06 19:54:40 -0500

    Add some more

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::path::Path;
 use clap::{Arg, App, SubCommand};
 
 fn main() {
@@ -18,15 +19,57 @@ fn main() {
             .default_value("public"))
         .get_matches();
 
-    let source = match matches.value_of("source_dir") {
+    let source_dir = match matches.value_of("source_dir") {
         Some(string) => string,
         None => panic!("Source directory cannot be null!"),
     };
-    let output = match matches.value_of("output_dir") {
+    let output_dir = match matches.value_of("output_dir") {
         Some(string) => string,
         None => panic!("Output directory cannot be null!"),
     };
 
-    println!("Input directory is {}", source);
-    println!("Output directory is {}", output);
+    println!("Input directory name is {}", source_dir);
+    println!("Output directory name is {}", output_dir);
+
+    fs::create_dir(output_dir);
+
+    println!("Input directory is {:?}", source_dir);
+    println!("Output directory is {:?}", output_dir);
+
+    let mut do_generate = Vec::new();
+
+    for emote in fs::read_dir(source_dir).unwrap() {
+        let emote = emote.unwrap();
+        let path = emote.path();
+        let extension = path.extension();
+        if extension == None {
+            continue
+        }
+        let extension = extension.unwrap().to_str().unwrap();
+
+        match extension {
+            "png" | "jpg" | "gif" => extension,
+            _ => continue
+        };
+        let name = path.file_name().unwrap();
+
+        let emote = Path::new(source_dir).join(name);
+        let out = Path::new(output_dir).join(name);
+
+        println!("out is {:?}", out);
+        if out.is_file() {
+            let in_last_modified = fs::metadata(&emote).unwrap().modified().unwrap();
+            let out_last_modified = fs::metadata(&out).unwrap().modified().unwrap();
+            if in_last_modified > out_last_modified {
+                do_generate.push(&name);
+            } else {
+                println!("Skipping {:?}!", name);
+            }
+        } else {
+            do_generate.push(&name);
+        }
+    }
+
+    println!();
+    println!("Files to generate: {:?}", do_generate);
 }
